871b275e87e69f111814510652930ffaf3650288
uni51 <unieye51@gmail.com>
2023-02-28 08:24:23 +0900

    11 登録済みの todo を編集可能にする

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { useState } from 'react';
 
-// Todo 型の定義
 type Todo = {
-  // プロパティ value は文字列型
   value: string;
   readonly id: number;
 };
 
 export const App = () => {
-  // 初期値: ''  （空文字列）のステート
   const [text, setText] = useState('');
   const [todos, setTodos] = useState<Todo[]>([]);
 
@@ -17,27 +14,32 @@ export const App = () => {
   };
 
   const handleSubmit = () => {
-    // 何も入力されていなかったらリターン
     if (!text) return;
-    // 新しい Todo を作成
-    // 明示的に型注釈を付けてオブジェクトの型を限定する
+
     const newTodo: Todo = {
       value: text,
       id: new Date().getTime(),
     };
 
-    /**
-     * スプレッド構文を用いて todos ステートのコピーへ newTodo を追加する
-     * 以下と同義
-     *
-     * const copyTodos = todos.slice();  // 配列をコピー
-     * copyTodos.unshift(newTodo);  // 配列の先頭に要素を追加
-     * setTodos(copyTodos);
-     *
-     **/
     setTodos([newTodo, ...todos]);
-    // フォームへの入力をクリアする
     setText('');
+  };
+
+  const handleEdit = (id: number, value: string) => {
+    /**
+     * 引数として渡された todo の id が一致する
+     * todos ステート（のコピー）内の todo の
+     * value プロパティを引数 value (= e.target.value) に書き換える
+     */
+    const newTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.value = value;
+      }
+      return todo;
+    });
+
+    // todos ステートを更新
+    setTodos(newTodos);
   };
 
   return (
@@ -48,18 +50,20 @@ export const App = () => {
           handleSubmit();
         }}
       >
-        <input
-          type="text"
-          // text ステートが持っている入力中テキストの値を value として表示
-          value={text}
-          // onChange イベント（＝入力テキストの変化）を text ステートに反映する
-          onChange={(e) => handleChange(e)}
-        />
+        <input type="text" value={text} onChange={(e) => handleChange(e)} />
         <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
       <ul>
         {todos.map((todo) => {
-          return <li key={todo.id}>{todo.value}</li>;
+          return (
+            <li key={todo.id}>
+              <input
+                type="text"
+                value={todo.value}
+                onChange={(e) => handleEdit(todo.id, e.target.value)}
+              />
+            </li>
+          );
         })}
       </ul>
     </div>
