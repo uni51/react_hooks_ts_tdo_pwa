b7c1ac7fe914b6e6f3a36d896063c7a4f3c33bbd
uni51 <unieye51@gmail.com>
2023-02-28 18:47:09 +0900

    19 ソースコードをコンポーネントごとに分割する

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import { useState } from 'react';
+import { FormDialog } from './FormDialog';
+import { ActionButton } from './ActionButton';
+import { SideBar } from './SideBar';
+import { TodoItem } from './TodoItem';
 
 type Todo = {
   value: string;
@@ -59,26 +63,6 @@ export const App = () => {
     setFilter(filter);
   };
 
-  const filteredTodos = todos.filter((todo) => {
-    // filter ステートの値に応じて異なる内容の配列を返す
-    switch (filter) {
-      case 'all':
-        // 削除されていないもの
-        return !todo.removed;
-      case 'checked':
-        // 完了済 **かつ** 削除されていないもの
-        return todo.checked && !todo.removed;
-      case 'unchecked':
-        // 未完了 **かつ** 削除されていないもの
-        return !todo.checked && !todo.removed;
-      case 'removed':
-        // 削除済みのもの
-        return todo.removed;
-      default:
-        return todo;
-    }
-  });
-
   const handleEmpty = () => {
     // シャローコピーで事足りる
     const newTodos = todos.filter((todo) => !todo.removed);
@@ -87,62 +71,10 @@ export const App = () => {
 
   return (
     <div>
-      <select
-        defaultValue="all"
-        onChange={(e) => handleSort(e.target.value as Filter)}
-      >
-        <option value="all">すべてのタスク</option>
-        <option value="checked">完了したタスク</option>
-        <option value="unchecked">現在のタスク</option>
-        <option value="removed">ごみ箱</option>
-      </select>
-      {/* フィルターが `removed` のときは「ごみ箱を空にする」ボタンを表示 */}
-      {filter === 'removed' ? (
-        <button
-          onClick={handleEmpty}
-          disabled={todos.filter((todo) => todo.removed).length === 0}
-        >
-          ごみ箱を空にする
-        </button>
-      ) : (
-        // フィルターが `checked` でなければ入力フォームを表示
-        filter !== 'checked' && (
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            <input type="text" value={text} onChange={(e) => handleChange(e)} />
-            <input type="submit" value="追加" onSubmit={handleSubmit} />
-          </form>
-        )
-      )}
-      <ul>
-        {filteredTodos.map((todo) => {
-          return (
-            <li key={todo.id}>
-              <input
-                type="checkbox"
-                disabled={todo.removed}
-                checked={todo.checked}
-                onChange={() => handleTodo(todo.id, 'checked', !todo.checked)}
-              />
-              <input
-                type="text"
-                disabled={todo.checked || todo.removed}
-                value={todo.value}
-                onChange={(e) => handleTodo(todo.id, 'value', e.target.value)}
-              />
-              <button
-                onClick={() => handleTodo(todo.id, 'removed', !todo.removed)}
-              >
-                {todo.removed ? '復元' : '削除'}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+      <SideBar onSort={handleSort} />
+      <FormDialog text={text} onChange={handleChange} onSubmit={handleSubmit} />
+      <TodoItem todos={todos} filter={filter} onTodo={handleTodo} />
+      <ActionButton todos={todos} onEmpty={handleEmpty} />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { App } from './App';
-import './index.css';
 
 // Element 型としてアサーション
 const root = createRoot(document.getElementById('root') as Element);
